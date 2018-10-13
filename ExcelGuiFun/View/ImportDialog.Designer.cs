8ef1eb5f22056334e78fd4b4a59638da851438b2
ll9 <llwierer@web.de>
2018-10-13 22:17:24 +0200

    Basic GUI

---
+++ ExcelGuiFun/View/ImportDialog.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace ExcelGuiFun
+{
+    partial class ImportDialog
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wizardControl1 = new AeroWizard.WizardControl();
+            this.PathPage = new AeroWizard.WizardPage();
+            this.CoordinatePage = new AeroWizard.WizardPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.wizardControl1)).BeginInit();
+            this.PathPage.SuspendLayout();
+            this.CoordinatePage.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // wizardControl1
+            // 
+            this.wizardControl1.BackColor = System.Drawing.Color.White;
+            this.wizardControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wizardControl1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wizardControl1.Location = new System.Drawing.Point(0, 0);
+            this.wizardControl1.Name = "wizardControl1";
+            this.wizardControl1.Pages.Add(this.PathPage);
+            this.wizardControl1.Pages.Add(this.CoordinatePage);
+            this.wizardControl1.Size = new System.Drawing.Size(376, 445);
+            this.wizardControl1.TabIndex = 0;
+            this.wizardControl1.Text = "Wizard Title";
+            // 
+            // PathPage
+            // 
+            this.PathPage.Controls.Add(this.textBox1);
+            this.PathPage.Controls.Add(this.button1);
+            this.PathPage.Name = "PathPage";
+            this.PathPage.Size = new System.Drawing.Size(329, 291);
+            this.PathPage.TabIndex = 0;
+            this.PathPage.Text = "Pfad";
+            // 
+            // CoordinatePage
+            // 
+            this.CoordinatePage.Controls.Add(this.button2);
+            this.CoordinatePage.Controls.Add(this.radioButton1);
+            this.CoordinatePage.Controls.Add(this.comboBox3);
+            this.CoordinatePage.Controls.Add(this.comboBox2);
+            this.CoordinatePage.Controls.Add(this.comboBox1);
+            this.CoordinatePage.Controls.Add(this.label3);
+            this.CoordinatePage.Controls.Add(this.label2);
+            this.CoordinatePage.Controls.Add(this.label1);
+            this.CoordinatePage.Name = "CoordinatePage";
+            this.CoordinatePage.Size = new System.Drawing.Size(329, 291);
+            this.CoordinatePage.TabIndex = 1;
+            this.CoordinatePage.Text = "Koordinaten";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(227, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(163, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(167, 28);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(167, 66);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 23);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(167, 107);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 23);
+            this.comboBox3.TabIndex = 5;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(43, 162);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(94, 19);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(213, 162);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // ImportDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 445);
+            this.Controls.Add(this.wizardControl1);
+            this.Name = "ImportDialog";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.wizardControl1)).EndInit();
+            this.PathPage.ResumeLayout(false);
+            this.PathPage.PerformLayout();
+            this.CoordinatePage.ResumeLayout(false);
+            this.CoordinatePage.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AeroWizard.WizardControl wizardControl1;
+        private AeroWizard.WizardPage PathPage;
+        private AeroWizard.WizardPage CoordinatePage;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+

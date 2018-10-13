8d90b330a89784403092f3cc5f322d623bb9a4e3
ll9 <llwierer@web.de>
2018-10-13 23:50:12 +0200

    Defined DataType Enum

---
+++ ExcelGuiFun/Models/DataTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelGuiFun.Models
+{
+    public enum DataTypes
+    {
+        System_Bool = 1,
+        System_Date = 2,
+        System_Numeric = 4,
+        System_Text = 8,
+    }
+}

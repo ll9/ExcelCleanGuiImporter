0f7961dd67ab60b0d3eeb10434fb904e240b0685
ll9 <llwierer@web.de>
2018-10-14 12:50:39 +0200

    TypeSafeTableConverter Fail

---
+++ ExcelGuiFunUnitTests/Utils/TypeSafeDataTableConverterTests.cs
@@ -0,0 +1,36 @@
+﻿using ExcelGuiFun.Utils;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelGuiFunUnitTests.Utils
+{
+    [TestFixture]
+    class TypeSafeDataTableConverterTests
+    {
+        [Test]
+        public void Convert_WhenCalled()
+        {
+            var table = new DataTable();
+            table.Columns.Add("1");
+            var row1 = table.NewRow();
+            var row2 = table.NewRow();
+            var row3 = table.NewRow();
+
+            row1[0] = 1;
+            row2[0] = 27;
+            row3[0] = DBNull.Value;
+
+            table.Rows.Add(row1);
+            table.Rows.Add(row2);
+            table.Rows.Add(row3);
+
+            var result = TypeSafeDataTableConverter.Convert(table);
+            Assert.That(result.Columns[0].DataType, Is.EqualTo(typeof(long?)));
+        }
+    }
+}

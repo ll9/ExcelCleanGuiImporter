47edd3fc8e2742e6233cb1d8ac0e93ed5498dd9f
ll9 <llwierer@web.de>
2018-10-14 16:11:31 +0200

    Refactoring

--- ExcelGuiFunUnitTests/Utils/DbBuilderTests.cs
+++ ExcelGuiFunUnitTests/Utils/DbBuilderTests.cs
@@ -13,13 +13,19 @@ namespace ExcelGuiFunUnitTests.Utils
     [TestFixture]
     class DbBuilderTests
     {
+        private Mock<ISqliteService> sqliteService;
+        private DbBuilder dbBuilder;
+
+        [SetUp]
+        public void Initialize()
+        {
+            sqliteService = new Mock<ISqliteService>();
+            dbBuilder = new DbBuilder("table", sqliteService.Object);
+        }
 
         [Test]
         public void CreateTable_WhenCalled_CreatesTable()
         {
-            var sqliteService = new Mock<ISqliteService>();
-            var dbBuilder = new DbBuilder("table", sqliteService.Object);
-
             var table = new DataTable();
             table.Columns.Add("boolColumn", typeof(bool));
             table.Columns.Add("dateColumn", typeof(DateTime));
@@ -31,6 +37,12 @@ namespace ExcelGuiFunUnitTests.Utils
             dbBuilder.CreateTable(table);
             sqliteService.Verify(x =>
                 x.ExecuteQuery("CREATE TABLE table (boolColumn BOOLEAN, dateColumn DATETIME, longColumn BIGINT, textColumn TEXT, noTypeColumn TEXT)", null));
+        }
+
+        [Test]
+        public void InsertData_WhenCalled_InsertsData()
+        {
+
         }
     }
 }

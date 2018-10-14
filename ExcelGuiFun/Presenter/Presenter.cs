2a3e0f146e4eca1f258b0600533672e980b0a935
ll9 <llwierer@web.de>
2018-10-14 09:57:28 +0200

    Presenter Get DataTable

--- ExcelGuiFun/Presenter/Presenter.cs
+++ ExcelGuiFun/Presenter/Presenter.cs
@@ -1,6 +1,8 @@
-﻿using ExcelGuiFun.View;
+﻿using ExcelGuiFun.Utils;
+using ExcelGuiFun.View;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +11,19 @@ namespace ExcelGuiFun.Presenter
 {
     class Presenter
     {
+        private DataTable _dataTable;
+
         public Presenter(IView view)
         {
             View = view;
+
+            view.ReadingExcel += GetDataTable;
+        }
+
+        private void GetDataTable(object sender, EventArgs e)
+        {
+            var reader = new ExcelReader(View.Path);
+            _dataTable = reader.ExtractDataTable();
         }
 
         public IView View { get; }
--- IntegrationTests/PresenterTest.cs
+++ IntegrationTests/PresenterTest.cs
@@ -2,6 +2,7 @@
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,13 @@ namespace IntegrationTests
     {
         public static void Main()
         {
+            var path = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            var excelFilePath = path + "\\hugedummy.xlsx";
+
             var gui = new Mock<IView>();
+            gui.SetupGet(view => view.Path).Returns(excelFilePath);
+            gui.SetupGet(view => view.XCoordinate).Returns("PLZ");
+            gui.SetupGet(view => view.YCoordinate).Returns("PLZ");
         }
     }
 }

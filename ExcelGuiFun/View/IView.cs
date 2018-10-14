adc6205c85c3821bdacc611a88c20fffe3638226
ll9 <llwierer@web.de>
2018-10-14 09:16:52 +0200

    Declared IView

---
+++ ExcelGuiFun/View/IView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelGuiFun.View
+{
+    interface IView
+    {
+        string Path { get; }
+        string Projektion { get; }
+        string XCoordinate { get; }
+        string YCoordinate { get; }
+        bool HasCoordinates { get; }
+    }
+}
